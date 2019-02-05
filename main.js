ed3727745b2082c78b259a7831d659a2a6af62f2
Aaron Raimist <aaron@raim.ist>
2019-02-05 13:08:49 -0600

    Visible false demo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -48,3 +48,16 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+const template = [
+  {
+    label: 'View',
+    submenu: [
+	  { role: 'zoomin' },
+      { role: 'zoomout', visible: false },
+    ],
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
